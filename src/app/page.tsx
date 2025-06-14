f1a3fe13b769c7af5c0ea7eac34b2f2acefcbf84
Rajeep-Gadal <rajeepgadal@gmail.com>
2025-06-14 10:38:37 +0545

    final update with code improvemnet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import FlipClock from "@/components/FlipClock";
 import { ModeToggle } from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
